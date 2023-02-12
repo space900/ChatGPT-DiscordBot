6b9f0c4e596a563db9089c9141d5536105c1d510
space900 <beyondmyspace@yandex.ru>
2023-02-12 10:46:34 +0300

    added aibot for discord server

---
+++ bot.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const {Client, GatewayIntentBits} = require("discord.js");
+const client = new Client({ 
+    intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+	],
+});
+
+const { Configuration, OpenAIApi} = require('openai');
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async function(message){
+    try {
+        if(message.author.bot) return;
+        const gptResponse = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `ChatGPT, is a friendly chatbot.\n\
+            ChatGPT: Hello, how are you?\n\
+            ${message.author.username}: ${message.content}\n\
+            ChatGPT:`,
+            temperature: 0.4,
+            max_tokens: 800,
+            stop: ["ChatGPT:", "space9000:"],
+        })
+
+        message.reply(`${gptResponse.data.choices[0].text}`);
+        return;
+
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
